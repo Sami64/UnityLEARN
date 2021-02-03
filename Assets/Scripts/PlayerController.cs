fdcee0ae6a4f00b792c25c31cf0225bf65ded146
Samuel Kow Paintsil <smashbros54@gmail.com>
2021-02-03 01:15:59 +0000

    initial

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    float speed = 30f;
+    Rigidbody playerRb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        playerRb.AddForce(Vector3.forward * speed * vertical, ForceMode.Acceleration);
+        playerRb.AddForce(Vector3.right * speed * horizontal, ForceMode.Acceleration);
+    }
+}
